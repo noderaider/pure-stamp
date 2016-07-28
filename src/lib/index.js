a60e7a34ecffef50b8346601885891cb57528a45
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-07-28 14:10:50 -0700

    documented

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,14 @@ import reactStamp from 'react-stamp'
 
 const freezeValue = value => ({ value, enumerable: true, writable: false, configurable: false })
 
-/** Creates an optimized pure compose function with dependencies tacked onto it for easy lib propagation. */
+/**
+ * Creates an optimized pure compose function with dependencies tacked onto it for easy lib propagation.
+ * @param  {Object}         options.React           React dependency.
+ * @param  {Object}         options.shallowCompare  React shallowCompare add-on dependency.
+ * @param  {Array<Object>}  options.deps            Optional dependencies to propagate.
+ * @param  {Object}         defaults                Optional configuration object to propagate.
+ * @return {function}                                 pure compose function with propagated dependencies and defaults.
+ */
 export default function pureStamp({ React, shallowCompare, ...deps } = {}, defaults = {}) {
   const { PropTypes, cloneElement } = React
   const { compose } = reactStamp(React)
