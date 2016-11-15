8dee52af2b52d96cb2b266acb04c9d59ac3d350a
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-11-15 14:30:28 -0800

    added pure.profile for getting information out of components

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,42 @@
 import reactStamp from 'react-stamp'
+import _ from 'lodash'
+
+
+function isRequiredUpdateObject(o) {
+  return Array.isArray(o) || (o && o.constructor === Object.prototype.constructor)
+}
+function deepDiff(o1, o2, p) {
+  const notify = (status) => {
+    console.warn('Update %s', status)
+    console.log('%cbefore', 'font-weight: bold', o1)
+    console.log('%cafter ', 'font-weight: bold', o2)
+  }
+  if (!_.isEqual(o1, o2)) {
+    console.group(p)
+    if ([ o1, o2 ].every( (o) => typeof o === 'function' )) {
+      notify('avoidable?')
+    } else if (![ o1, o2 ].every(isRequiredUpdateObject)) {
+      notify('required.')
+    } else {
+      const keys = _.union(Object.keys(o1), Object.keys(o2))
+      for (const key of keys) {
+        deepDiff(o1[key], o2[key], key)
+      }
+    }
+    console.groupEnd()
+  } else if (o1 !== o2) {
+    console.group(p)
+    notify('avoidable!')
+    if (_.isObject(o1) && _.isObject(o2)) {
+      const keys = _.union(Object.keys(o1), Object.keys(o2))
+      for (const key of keys) {
+        deepDiff(o1[key], o2[key], key)
+      }
+    }
+    console.groupEnd()
+  }
+}
+
 
 const freezeValue = value => ({ value, enumerable: true, writable: false, configurable: false })
 
@@ -14,28 +52,54 @@ export default function pureStamp({ React, shallowCompare, ...deps } = {}, defau
   const { PropTypes, cloneElement } = React
   const { compose } = reactStamp(React)
 
-
-  const pure = Object.entries({ React
-                              , PropTypes
-                              , cloneElement
-                              , ...deps
-                              , defaults
-                              , impure: compose
-                              , destructure: () => [ { React, shallowCompare, ...deps }, defaults ]
-                              })
-    .reduce (
-      ( pure, [ depName, dep ]) => Object.defineProperty( pure, depName, { value: dep, configurable: false, enumerable: true })
-    , function pure (...desc) {
-        const Stamp = compose(
-          { displayName: 'PureComponent'
-          , shouldComponentUpdate(nextProps, nextState) {
-              return shallowCompare(this, nextProps, nextState)
-            }
-          }
-          , ...desc
-        )
-        return Stamp
+  const pureDesc = (
+    { displayName: 'PureComponent'
+    , shouldComponentUpdate(nextProps, nextState) {
+        return shallowCompare(this, nextProps, nextState)
       }
-    )
+    }
+  )
+
+  const profileDesc = (
+    { componentDidUpdate (prevProps, prevState) {
+        deepDiff (
+          { props: prevProps
+          , state: prevState
+          }
+        , { props: this.props
+          , state: this.state
+          }
+        , this.constructor.displayName
+        )
+      }
+    }
+  )
+
+  function _pure (...desc) {
+    return compose( pureDesc, ...desc )
+  }
+  function impure (...desc) {
+    return compose( ...desc )
+  }
+  function profile (...desc) {
+    return compose ( pureDesc, profileDesc, ...desc )
+  }
+  function impureProfile (...desc) {
+    return compose ( profileDesc, ...desc )
+  }
+  const pure = Object.entries(
+    { React
+    , PropTypes
+    , cloneElement
+    , ...deps
+    , defaults
+    , impure
+    , profile
+    , impureProfile
+    , destructure: () => [ { React, shallowCompare, ...deps }, defaults ]
+    }
+  ).reduce ( ( pure, [ depName, dep ] ) => Object.defineProperty( pure, depName, { value: dep, configurable: false, enumerable: true })
+  , _pure
+  )
   return Object.freeze(pure)
 }
